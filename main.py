bcb207ed8c6095d24220d73e8a23c7a5913bd890
kenzoclauw@gmail.com <kenzoclauw@gmail.com>
2018-04-29 19:14:42 +0200

    created dataset

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ try:
     cmap = pycpt.load.cmap_from_cptcity_url('ukmo/wow/temp-c.cpt')
 except:
     cmap = 'Spectral_r'
+
+mag_data = pd.read_csv('./data/processed/mag.csv', index_col=0)
+mag_data['geometry'] = [geometry.Point(x, y) for x, y in zip(mag_data['long_wgs84'], mag_data['lat_wgs84'])]
+mag_data = gpd.GeoDataFrame(mag_data, geometry='geometry', crs="+init=epsg:4326")
+
+mag_data.head()
